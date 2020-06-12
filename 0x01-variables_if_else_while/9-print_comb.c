6e3394a714b54fcd836c75e26180d6642cd7fcd4
Your Name <crisbed321@gmail.com>
2020-06-12 07:02:44 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	char cma = ',';
+	char esp = ' ';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		if (ch <= '8' )
+		{
+			putchar(ch);
+			putchar(cma);
+			putchar(esp);
+		}
+		else if (ch == '9')
+			putchar(ch);
+	}
+	return (0);
+
+}
