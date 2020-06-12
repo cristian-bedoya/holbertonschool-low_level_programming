b8706ae14955fecc860294475c5291246f6c82d0
Your Name <crisbed321@gmail.com>
2020-06-12 05:35:49 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,31 +3,24 @@
 #include <stdio.h>
 
 /**
- *main-random number is positive, negative or zero
+ * main - Uses if...else if...else statements with relational operators
  *
- *Return:0
+ * Return: 0
  */
-
 int main(void)
 {
-        int n;
-        int ld;
+	int n, lastDigit;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        ld = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        if (ld > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n",n,ld);
-        }
-        else if (ld == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n",n,ld);
-        }
-        else if (ld < 6 && ld != 0)
-	     {
-		     printf("Last digit of %d is %d and less than 6 and not 0\n",n,ld);
-	     }
-        return (0);
+	lastDigit = n % 10;
+	printf("Last digit of %i is %i ", n, lastDigit);
+	if (lastDigit > 5)
+		printf("and is greater than 5\n");
+	else if (lastDigit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
