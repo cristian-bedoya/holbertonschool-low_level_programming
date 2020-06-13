2d2b43bda27ea4bb0fad463bf206c2f59dad6b2b
Your Name <crisbed321@gmail.com>
2020-06-13 05:36:24 +0000

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+	int n3;
+
+	for (n1 = 48 ; n1 <= 57 ; n1++)
+	{
+		for (n2 = 48 ; n2 <= 57 ; n2++)
+		{
+			for (n3 = 48 ; n3 <= 57 ; n3++)
+			{
+				if (n1 < n2)
+				{
+					if (n2 < n3)
+					{
+						putchar(n1);
+						putchar(n2);
+						putchar(n3);
+						if (n1 != 55 || n2 != 56 || n3 != 57)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
