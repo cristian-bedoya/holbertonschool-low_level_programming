8dbd6de1598c0128b77dba5e5d1f5aba11c6646b
cristian <crisbed321@gmail.com>
2020-06-24 05:06:42 +0000

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,21 +3,21 @@
 /**
  * print_array - prints n elements of an array of integers
  * @n:elements of arrays
- * @a: pointer to int
+ * @i: pointer to int
  * Return: nothing
  */
 
 void print_array(int *a, int n)
 {
-	int a;
+	int i;
 
-	a = 0;
+	i = 0;
 
-	while (a < n)
+	while (i < n)
 	{
-		printf("%d", a[a]);
-		a++;
-		if (a != n)
+		printf("%d", i[i]);
+		i++;
+		if (i != n)
 		{
 			printf(", ");
 		}
