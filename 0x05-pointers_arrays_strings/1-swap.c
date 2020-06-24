ec30aad96fcdba13d9f4e6e4f64d9a9702e62c6c
cristian <crisbed321@gmail.com>
2020-06-24 01:40:46 +0000

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - updates the values of variable
- * @a: pointer 1  variable
+ * swap_int  - swap values of integers
+ * @a: pointer 1 variable
  * @b: pointer 2 varibale
  */
 void swap_int(int *a, int *b)
