eb5bf986f3ca79c917601f3dc44db579b806259a
cristian <crisbed321@gmail.com>
2020-06-29 17:32:54 +0000

    task 2.1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * *_strchr - locates a character in a string
@@ -16,6 +17,10 @@ char *_strchr(char *s, char c)
 		{
 			return (s);
 		}
+		else
+			{
+				return (NULL);
+			}
+		}
 	}
-	return ('\0');
 }
