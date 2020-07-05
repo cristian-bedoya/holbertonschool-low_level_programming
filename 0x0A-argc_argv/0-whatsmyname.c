7c48202fb87171dac60e8d489759ec7a76c2d237
cristian <crisbed321@gmail.com>
2020-07-05 00:05:00 +0000

    task 0.1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
-        while(argc--)
-                printf("%s\n", *argv++);
-        exit(EXIT_SUCCESS);
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
 }
