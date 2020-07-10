3acd68b235df3350d4d2ba2fe3ea6ff3e87c07ea
cristian <crisbed321@gmail.com>
2020-07-10 01:35:24 +0000

    1 fixxed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,38 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int lens1, lens2, i, j;
-	char *str;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	for (lens1 = 0; s1[lens1] != '\0'; lens1++)
-		;
-	for (lens2 = 0; s1[lens2] != '\0'; lens2++)
-		;
-
-	if (n >= lens2)
-		str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		str = malloc(sizeof(char) * (lens1 + 1 + n));
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (str == NULL)
+	if (!s)
 		return (NULL);
 
-	for (i = 0; i < lens1; i++)
-		str[i] = s1[i];
-	for (j = 0; n >= lens2 && i < (lens1 + lens2);)
+	while (i < len1)
 	{
-		str[i++] = s2[j++];
-	}
-	for (j = 0; n < lens2 && i < (lens1 + n);)
-	{
-		str[i++] = s2[j++];
+		s[i] = s1[i];
+		i++;
 	}
 
-	str[i] = '\0';
-	return (str);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
