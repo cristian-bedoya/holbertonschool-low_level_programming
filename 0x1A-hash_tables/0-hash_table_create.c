3196b5c160a3572819ebf5226d47748cefd074e5
cristian <crisbed321@gmail.com>
2020-10-02 20:38:17 +0000

    hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,27 +2,22 @@
 
 /**
  * hash_table_create - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * @size: size of the hash table
+ * Return: A new table.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
+	hash_table_t *new_table;
 
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+		return (NULL);
 
-    new_table = malloc(sizeof(hash_table_t));
-    if(!new_table)
-        return(NULL);
+	new_table->size = size;
 
-    new_table->size = size;
-    /*
-    new_table->array = calloc(new_table->size, sizeof(hash_node_t *));
-    */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if(!(new_table->array))
-        return(NULL);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!(new_table->array))
+		return (NULL);
 
-    return new_table;
+	return (new_table);
 }
-
