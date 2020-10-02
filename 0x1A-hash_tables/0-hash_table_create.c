bcfc0060181a2277c8ee6412b068e7670bcf0969
cristian <crisbed321@gmail.com>
2020-10-02 20:49:26 +0000

    inicilizitaion of hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	size_t i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
@@ -18,6 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(new_table->array))
 		return (NULL);
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
